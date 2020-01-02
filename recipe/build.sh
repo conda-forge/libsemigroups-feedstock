777042299981da8e90c607cfdff4dd529e3d54b2
Isuru Fernando <isuruf@gmail.com>
2020-01-02 09:28:22 +0530

    Reduce parallellism in travis-ci

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,10 @@ if [[ "$target_platform" != *-64 ]]; then
   sed -i.bak 's/-march=native//g' Makefile.am
 fi
 
+if [[ "$CI" == "travis" ]]; then
+  export CPU_COUNT=4
+fi
+
 autoreconf -vif .              || exit 1
 ./configure --prefix="$PREFIX" || exit 1
 make -j${CPU_COUNT}            || exit 1
