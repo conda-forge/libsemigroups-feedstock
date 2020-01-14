3849ebbd92e8a28a2660deed7a870ac88aec0afb
Isuru Fernando <isuruf@gmail.com>
2020-01-14 13:23:08 +0530

    Disable hpcombi and enable external fmt

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-sed -i.bak 's/-mavx//g' Makefile.am
-sed -i.bak 's/-march=native//g' Makefile.am
-
 if [[ "$CI" == "travis" ]]; then
   export CPU_COUNT=4
 fi
 
 autoreconf -vif .              || exit 1
-./configure --prefix="$PREFIX" || exit 1
+./configure --prefix="$PREFIX" --with-external-fmt --disable-hpcombi || exit 1
 make -j${CPU_COUNT}            || exit 1
 make check -j${CPU_COUNT}      || exit 1
 make install
