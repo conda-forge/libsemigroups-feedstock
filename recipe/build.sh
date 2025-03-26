acdfa2885bf77c459207a939f12b04b2a2a0fd06
Joseph Edwards <josephdavidedwards@gmail.com>
2025-03-26 20:07:18 +0000

    Attempt to debug sims

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,9 +5,10 @@ if [[ "$CI" == "travis" ]]; then
 fi
 
 autoreconf -vif .              || exit 1
-./configure --prefix="$PREFIX" --with-external-fmt --with-external-eigen --disable-hpcombi || exit 1
+./configure --prefix="$PREFIX" --with-external-fmt --with-external-eigen --disable-hpcombi --enable-debug || exit 1
 make -j${CPU_COUNT}            || exit 1
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-make check -j${CPU_COUNT}      || exit 1
+make test_sims -j${CPU_COUNT}  || exit 1
+./test_sims "quick"
 fi
 make install
